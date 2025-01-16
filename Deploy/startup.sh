828141e5c63204ff0506d0dbf136ca22a6145048
perezsebastian21 <perezsebastian21@gmail.com>
2025-01-16 16:12:52 -0300

    se agregó el port

--- Deploy/startup.sh
+++ Deploy/startup.sh
@@ -2,7 +2,8 @@
 
 echo "v.0.0.1"
 sleep 5
+export PORT=${PORT:-8080}
+# Reemplazar ${PORT} en nginx.conf con el valor real del puerto
+envsubst '${PORT}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp
+mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
 service nginx start
-# Mantiene el proceso principal activo para evitar que Heroku marque el dyno como "crashed"
-echo "nginx is running. Keeping the process alive..."
-tail -f /var/log/nginx/access.log /var/log/nginx/error.log
