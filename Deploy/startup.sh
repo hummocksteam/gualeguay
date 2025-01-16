a1eaf3991e437ff8c0339027fe943ba3900e36e6
perezsebastian21 <perezsebastian21@gmail.com>
2025-01-16 15:53:34 -0300

    se agrego un logueo y un echo

--- Deploy/startup.sh
+++ Deploy/startup.sh
@@ -3,3 +3,6 @@
 echo "v.0.0.1"
 sleep 5
 service nginx start
+# Mantiene el proceso principal activo para evitar que Heroku marque el dyno como "crashed"
+echo "nginx is running. Keeping the process alive..."
+tail -f /var/log/nginx/access.log /var/log/nginx/error.log
