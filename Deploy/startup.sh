77e0cddeea559c085eac9e08dde499267d9a93b6
perezsebastian21 <perezsebastian21@gmail.com>
2025-01-16 16:28:49 -0300

    se agrega supuestamente para que no se caiga nginx

--- Deploy/startup.sh
+++ Deploy/startup.sh
@@ -7,3 +7,4 @@ export PORT=${PORT:-8080}
 envsubst '${PORT}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp
 mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
 service nginx start
+tail -f /var/log/nginx/access.log /var/log/nginx/error.log
